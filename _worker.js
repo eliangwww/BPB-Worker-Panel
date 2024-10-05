5f3d753bf6927cd4f3e84cf9b28e1b98d9c05b3e
eliangwww <96296131+eliangwww@users.noreply.github.com>
2024-10-05 10:05:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ let dohURL = 'https://cloudflare-dns.com/dns-query';
 let trojanPassword = `bpb-trojan`;
 // https://emn178.github.io/online-tools/sha224.html
 // https://www.atatus.com/tools/sha224-to-hash
-let hashPassword = '8b0587d61e85b4ab7dde3b438c16fe744b14368852ea85c2d05bd9c2';
+let hashPassword = '78b9d3c19a9bbce2aac88831914c463ee0f1d912d63fb3c00521a282';
 let panelVersion = '2.6.4';
 
 if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
